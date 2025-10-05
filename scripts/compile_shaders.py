aaaea8ad7df8f4787cd9e4a0a95d1ac044b00bb8
Victor Dadaciu <victor.dadaciu@gmail.com>
2025-10-06 02:31:10 +0300

    Handle shader compilation from CMake
    I think I'm getting to obsessed with CMake. Let's do lighting

--- scripts/compile_shaders.py
+++ scripts/compile_shaders.py
@@ -19,13 +19,11 @@ def parse_args() -> None:
     args = vars(parser.parse_args(argv))
     global GLSL_DIR
     GLSL_DIR = os.path.abspath(args["glsl"])
-    print(GLSL_DIR)
     if not (os.path.exists(GLSL_DIR) and os.path.isdir(GLSL_DIR)):
         parser.error("Input path must be a valid directory")
     spirv: str | None = args["spirv"]
     global SPIRV_DIR
     SPIRV_DIR = GLSL_DIR if spirv is None else os.path.abspath(spirv)
-    print(GLSL_DIR, SPIRV_DIR)
 
 def to_glsl_file(filename: str) -> str:
     return os.path.join(GLSL_DIR, filename)
